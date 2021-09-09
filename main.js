3aaad7ac03ca303f038201d7f0233e59d9314329
sebastian1134 <sebastianxaguilera@gmail.com>
2021-09-09 19:54:36 -0300

    siete

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const phrases = {
     _first: ['Yesterday was a', 'Today is a', 'Tomorrow will be a'],
     _second: ['sunny day', 'cloudy day', 'rainy day'],
-    _third: ['ideal for studying.','ideal for Netflix.', 'ideal for doing sports.']
+    _third: ['ideal for studying.','ideal for Netflix.', 'ideal for doing sports.'] 
+};
+const randomNumbert = () => {
+    return Math.floor(Math.random()*3);
 };
 
 const phrasePicker = () => {
-    let randomNumber = Math.floor(Math.random()*3);
-    return `${phrases._first[randomNumber]} \n${phrases._second[randomNumber]} ${phrases._third[randomNumber]}`;
+    let sndRandomNum = Math.floor(Math.random()*2);
+    let first = randomNumbert();
+    let second = randomNumbert();
+    let third = randomNumbert();
+    if (second != 0) {
+        third = sndRandomNum;
+    }
 
+    return `${phrases._first[first]} \n${phrases._second[second]} ${phrases._third[third]}`;
 };
 
 const figlet = require('figlet');
